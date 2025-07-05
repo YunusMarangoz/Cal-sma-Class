da1766cfe396f967cda317c181d7b234c1a189e2
Yunus <yunusmarangoz@gmail.com>
2025-07-06 00:50:01 +0300

    Add project files.

---
+++ Calısma Class/Arac.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calısma_Class
+{
+    public class Arac
+    {
+        public string Marka;
+        public string Model;
+        public int ModelYil;
+        public int Yakittipi;
+        public string? Vitestipi;
+        public decimal AlısFiyat;
+        public decimal SatisFiyat;
+        public decimal MaxindirimTutar;
+        public decimal Fiyat;
+        public Arac() 
+        { }
+       public Arac(string _Marka,string _Model)
+        { 
+            Marka = _Marka;
+            Model = _Model; 
+        }
+        public Arac(string _Marka, string _Model,int _modelYil)
+        {
+            Marka = _Marka;
+            Model = _Model;
+            ModelYil = _modelYil;
+        }
+        public Arac(string marka, string model, int modelYil, int yakittipi, string vitestipi, decimal alısFiyat, decimal satisFiyat, decimal maxindirimTutar, decimal fiyat) : this(marka, model)
+        {
+            ModelYil = modelYil;
+            Yakittipi = yakittipi;
+            Vitestipi = vitestipi;
+            AlısFiyat = alısFiyat;
+            SatisFiyat = satisFiyat;
+            MaxindirimTutar = maxindirimTutar;
+            Fiyat = fiyat;
+        }
+        public void BilgileriGoruntule()
+        {
+            Console.WriteLine( $"MArka: {Marka}, Model: {Model}, Model Yıl: {ModelYil}");
+        }
+        public void FiyatAta(decimal _fiyat)
+        {
+            decimal Fiyathesap = SatisFiyat - MaxindirimTutar;
+            if (_fiyat < Fiyathesap)
+            {
+                Console.WriteLine( "Geçersiz Fiyat Girişi Yaptınız");
+            }
+            else
+                Fiyat=_fiyat;
+        }
+    }
+}
---
+++ Calısma Class/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Calısma_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sınıflar!");
+            Arac arac = new Arac("Opel", "Corsa", 2025);
+            arac.AlısFiyat = 400000;
+            arac.SatisFiyat = 450000;
+            arac.MaxindirimTutar = 25000;
+            arac.FiyatAta(430000);
+            arac.BilgileriGoruntule();
+        }
+    }
+}
